c6d348cd9dc1e886025228d878d1de7e3802c1f9
cristina <cristina.outeda.rua@gmail.com>
2021-10-12 14:52:25 +0200

    added event type config

---
+++ eventType.js
@@ -0,0 +1,22 @@
+import avro from "avsc";
+
+export default avro.Type.forSchema({
+  type: "record",
+  fields: [
+    {
+      name: "damage",
+      type: { type: "enum", symbols: ["follenTree", "shit", "brokenTile"] },
+    },
+    {
+      name: "geolocation",
+      type: {
+        type: "map",
+        values: "float",
+      },
+    },
+    {
+      name: "date",
+      type: "float",
+    },
+  ],
+});
